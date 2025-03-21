d1b28b1343e3855afec5db123ee323b3adc68509
easel7 <blankeasel7@gmail.com>
2025-03-21 10:45:12 +0100

    update the script

--- Macro/run_power.sh
+++ Macro/run_power.sh
@@ -1,8 +1,8 @@
-exe=/Users/xiongzheng/software/B4/B4c/build/exampleB4c
+exe=/Users/xiongzheng/software/B4/B4e/build/exampleB4c
 Tag=(Proton Deuteron Electron Helium4 Carbon Helium3) #Proton Deuteron Electron Helium4 Carbon Helium3 
 
 for i in "${!Tag[@]}"; 
 do
-    echo  /Users/xiongzheng/software/B4/B4c/Macro/${Tag[$i]}_PowerLaw.mac
-    ${exe} -m  /Users/xiongzheng/software/B4/B4c/Macro/${Tag[$i]}_PowerLaw.mac
+    echo  /Users/xiongzheng/software/B4/B4e/Macro/${Tag[$i]}_PowerLaw.mac
+    ${exe} -m  /Users/xiongzheng/software/B4/B4e/Macro/${Tag[$i]}_PowerLaw.mac
 done
