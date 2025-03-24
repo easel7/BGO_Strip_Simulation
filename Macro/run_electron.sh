f397aec97ef69aeef8718268aaced032e7ccbfb6
easel7 <blankeasel7@gmail.com>
2025-03-24 11:52:03 +0100

    update

--- Macro/run_electron.sh
+++ Macro/run_electron.sh
@@ -2,12 +2,12 @@
 exe=/Users/xiongzheng/software/B4/B4e/build/exampleB4e
 Tag=Electron
 # Loop through energy values from 10 to 100 GeV with a step of 10
-for energy in $(seq 10 10 90)
-do
-    ${exe} -m ${Tag}_${energy}GeV.mac
-done
+# for energy in $(seq 10 10 90)
+# do
+#     ${exe} -m ${Tag}_${energy}GeV.mac
+# done
 
-for energy in $(seq 100 100 1000)
+for energy in $(seq 1000 100 1000)
 do
     ${exe} -m ${Tag}_${energy}GeV.mac
 done
