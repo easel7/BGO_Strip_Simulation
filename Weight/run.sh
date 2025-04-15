e02b939a1b8677e78204b5365e71610b2b83add6
easel7 <blankeasel7@gmail.com>
2025-04-15 11:29:36 +0200

    Update run.sh

--- Weight/run.sh
+++ Weight/run.sh
@@ -4,13 +4,13 @@ cp /Users/xiongzheng/software/B4/B4e/Root/Proton_PowerLaw.root     /Users/xiongz
 cp /Users/xiongzheng/software/B4/B4e/Root/Deuteron_PowerLaw.root   /Users/xiongzheng/software/B4/B4e/Weight/Deuteron_PowerLaw.root  
 cp /Users/xiongzheng/software/B4/B4e/Root/Helium3_PowerLaw.root    /Users/xiongzheng/software/B4/B4e/Weight/Helium3_PowerLaw.root   
 cp /Users/xiongzheng/software/B4/B4e/Root/Helium4_PowerLaw.root    /Users/xiongzheng/software/B4/B4e/Weight/Helium4_PowerLaw.root   
-# cp /Users/xiongzheng/software/B4/B4e/Root/Carbon_PowerLaw.root     /Users/xiongzheng/software/B4/B4e/Weight/Carbon_PowerLaw.root    
+cp /Users/xiongzheng/software/B4/B4e/Root/Carbon_PowerLaw.root     /Users/xiongzheng/software/B4/B4e/Weight/Carbon_PowerLaw.root    
 
 
 
 exe=/Users/xiongzheng/software/B4/B4e/Weight/Normalized
 filepath=/Users/xiongzheng/software/B4/B4e/Weight/
-Tag=(Helium3 Proton Deuteron Electron Helium4) # Carbon Helium3 Proton Deuteron Electron Helium4
+Tag=(Carbon Helium3 Proton Deuteron Electron Helium4) # Carbon Helium3 Proton Deuteron Electron Helium4
 for i in "${!Tag[@]}"; 
 do
     echo "${exe} ${Tag[$i]}_PowerLaw.root"
