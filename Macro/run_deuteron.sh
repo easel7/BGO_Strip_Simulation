cb8ffcd1ad12695744caa2dbb12e1b85f19b7474
easel7 <blankeasel7@gmail.com>
2025-05-07 16:33:35 +0200

    update

--- Macro/run_deuteron.sh
+++ Macro/run_deuteron.sh
@@ -2,12 +2,12 @@
 exe=/Users/xiongzheng/software/B4/B4e/build/exampleB4e
 Tag=Deuteron
 # Loop through energy values from 10 to 100 GeV with a step of 10
-# for energy in $(seq 10 10 90)
-# do
-#     ${exe} -m ${Tag}_${energy}GeV.mac
-# done
+for energy in $(seq 10 10 90)
+do
+    ${exe} -m ${Tag}_${energy}GeV.mac
+done
 
-for energy in $(seq 1000 100 1000)
+for energy in $(seq 100 100 900)
 do
     ${exe} -m ${Tag}_${energy}GeV.mac
 done
@@ -17,4 +17,4 @@ done
 #     ${exe} -m ${Tag}_${energy}GeV.mac
 # done
 
-${exe} -m  /Users/xiongzheng/software/B4/B4e/Macro/${Tag}_PowerLaw.mac
+# ${exe} -m  /Users/xiongzheng/software/B4/B4e/Macro/${Tag}_PowerLaw.mac
